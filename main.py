1a611a42227e10aa59a7aef8b208b93d0983094e
Sam Persinger <spersin00@gmail.com>
2026-01-26 12:32:31 -0500

    Add initial camera support, change aspect ratio

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
+from kivy.core.window import Window
+import time
 
-class MyWidget(BoxLayout):
+Window.size = (440,946)
+Config.set('kivy', 'camera', 'opencv')
+
+class Home(Screen): pass
+class Voice(Screen): pass
+class Scanner(Screen):
+    def on_enter(self):
+        self.ids.cam_view.ids.camera.play = True
+
+    def on_leave(self):
+        self.ids.cam_view.ids.camera.play = False
+
+class Edit(Screen): pass
+
+class Root(BoxLayout):
     pass
+
+
+class CameraClick(BoxLayout):
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        camera.export_to_png("IMG_{}.png".format(timestr))
+        print("Captured")
+
 
 class MyApp(App):
     def build(self):
-        return MyWidget()
+        return Root()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MyApp().run()
+
