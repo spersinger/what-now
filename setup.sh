28191693c37c2ea104ce456522a724801d466897
lavender-aa <sodaissoda@gmail.com>
2026-01-26 14:30:38 -0500

    added setup shell script for quick startup

--- setup.sh
+++ setup.sh
@@ -13,23 +13,34 @@ is_sourced() {
 setup_env() {
     # create the virtual environment with the correct python version
     python3.13 -m venv .env
-    echo "virtual environment created ($(pwd)/.env)"
+    echo -e "\nvirtual environment created ($(pwd)/.env)"
 
     # add .env/bin to PATH so that python and pip versions match env spec
     export PATH=$(pwd)/.env/bin:$PATH
-    echo "exported venv to current path (changes only persist in this terminal instance)"
+    echo -e "exported venv to current path (changes only persist in this terminal instance)\n"
 
     # install kivy
     pip install "kivy[full]"
-    echo "installed kivy."
+    echo -e "\ninstalled kivy."
 }
 
 main() {
-    is_sourced || (echo "This file must be sourced to work correctly (\`source setup.sh\`)." && exit 1) &&
-        (python3.13 --version ||
-            (echo "This project requires Python3.13 to install Kivy with. Please install that version and try again." && exit 2) &&
-            setup_env)
 
+    if [ -d ".env" ]; then
+        echo -e "\nThis file has already been sourced."
+        echo -e "To re-enable the virtual environment, use \`source .env/bin/activate\`."
+        echo -e "If there are still issues, delete the .env folder and re-source this file in a new terminal."
+        echo -e "Otherwise, if there are still issues, re-cloning the repo is always an option."
+        return 0
+    fi
+
+    is_sourced ||
+        (echo "This file must be sourced to work correctly (\`source setup.sh\`)." && exit 1) &&
+        (
+            command -v python3.13 ||
+                (echo "This project requires Python3.13 to install Kivy with. Please install that version and try again." && exit 2) &&
+                setup_env
+        )
 }
 
 main
