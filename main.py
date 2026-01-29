729cc54afa193d991910694d8577bcef91c4e23c
Sam Persinger <spersin00@gmail.com>
2026-01-29 12:51:21 -0500

    Remove more features that should be only on the camera branch

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.popup import Popup
-from kivy.uix.filechooser import FileChooserIconView
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.config import Config
 from kivy.core.window import Window
 
-import time
-import pytesseract
-import numpy as np
-import cv2
-from PIL import Image
-
 Window.size = (440,946)
-Config.set('input', 'mouse', 'mouse,disable_multitouch')
-Config.set('input', 'mtdev_%(name)s', '')
-Config.set('input', 'hid_%(name)s', '')
 Config.set('kivy', 'camera', 'opencv')
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '440')
@@ -40,16 +28,7 @@ class Root(BoxLayout):
 
 class CameraClick(BoxLayout):
     def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        file_name = "IMG_{}.png".format(timestr)
-        camera.export_to_png(file_name)
-
-        print("Captured")
+        print("Captured todo")
     def upload(self):
         '''
         Function to upload images from a camera roll or desktop
