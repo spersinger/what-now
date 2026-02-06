d13aaa8fd62ed746fdd6204c031ef756db514956
ethanpjanovich <ethanpjanovich@gmail.com>
2026-02-06 12:49:33 -0500

    added submit button that clears the voice input string and the text box (will eventually send the text to command interpreter), the textbox shows the voice input while recording and can be edited. if the voice screen is exited while recording, recording stops and the string and textbox are cleared. When recording is stopped the start recording button is disabled for 3 seconds.

--- main.py
+++ main.py
@@ -53,9 +53,10 @@ class Voice(Screen):
     def start_voice(self):
         mic_icon = self.ids.mic_icon
 
-
         if not self.listening:
             print("recording started")
+            #disable submit button when recording
+            self.ids.submit_voice_button.disabled = True
             # listening is started and event is not stopped
             self.listening = True
             self.stop_event.clear()
@@ -73,10 +74,14 @@ class Voice(Screen):
         else:
             print("recording stopped")
             self.listening = False
-
+            #disabled record button for 2 seconds
+            self.ids.record_button.disabled = True
+            Clock.schedule_once(lambda dt: setattr(self.ids.record_button, "disabled", False), 3)
             # change button text
             self.ids.record_button.text = "Record"
             mic_icon.source = "mic_white.png"  # change back to white
+            #enable submit button
+            self.ids.submit_voice_button.disabled = False
 
             self.stop_event.set()
 
@@ -85,10 +90,11 @@ class Voice(Screen):
             with sr.Microphone() as source:
                 # try to ignore background noise
                 r.adjust_for_ambient_noise(source, duration=0.2)
+
                 while not self.stop_event.is_set():
                     try:
                         # start recording, 5 seconds for a phrase
-                        audio = r.listen(source, phrase_time_limit= 4)
+                        audio = r.listen(source, phrase_time_limit= 5)
                         # using google for speech to text
                         text = r.recognize_google(audio)
 
@@ -106,6 +112,30 @@ class Voice(Screen):
         finally:
             self.listen_thread = None
             print("listen thread exited")
+
+    # submit buttons function -- will eventually send text to command interpreter
+    def submit_voice(self):
+        app = App.get_running_app()
+        app.voice_input = ""
+        self.ids.voice_text_input.text = ""
+    def on_leave(self):
+        # If we are recording, stop
+        if self.listening:
+            print("Voice screen left – stopping recording")
+
+            self.listening = False
+            self.stop_event.set()
+
+            # Reset voice screen
+            if 'mic_icon' in self.ids:
+                self.ids.mic_icon.source = "mic_white.png"
+            if 'record_button' in self.ids:
+                self.ids.record_button.text = "Record"
+            if 'voice_text_input' in self.ids:
+                self.ids.voice_text_input.text = ""
+                app = App.get_running_app()
+                app.voice_input = ""
+
 
 class Scanner(Screen):
 
