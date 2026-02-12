6b7c7f122b899c639d00b6ae6e82056aebf6d972
lavender-aa <sodaissoda@gmail.com>
2026-02-12 13:08:50 -0500

    initial structure of schedule

---
+++ src/Schedule.py
@@ -0,0 +1,20 @@
+from CalendarEvent import CalendarEvent
+from typing import List
+
+# contains all of a user's events
+# purpose: manage calendar events
+class Schedule():
+    events: List[List[CalendarEvent]] # repeating events grouped together
+    
+    def __init__(self):
+        events = []
+    
+    def add_event(self):
+        pass
+
+    def add_class(self):
+        pass
+
+    def add_reminder(self):
+        pass
+
