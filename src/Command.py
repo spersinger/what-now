e34996fb07a27492c28432f11e66a8a58b916250
lavender-aa <sodaissoda@gmail.com>
2026-03-02 00:57:12 -0500

    added docstring to Command, few other changes
    most changes related to work on how UI class will interact with schedule/CI

--- src/Command.py
+++ src/Command.py
@@ -1,8 +1,6 @@
-from CalendarEvent import CalendarEvent
+from CalendarEvent import CalendarEvent, Date
 from enum import Enum
 from typing import Tuple, List
-
-
 
 class CommandType(Enum):
     """Represents the different functions a Command can process."""
@@ -16,22 +14,40 @@ class CommandType(Enum):
 
 # command
 # holds information about one request to send to the schedule.
-# TODO: figure out how modifying events works
 class Command:
-    """Holds information about an event and what to do with it."""
+    """
+    Holds information about an event and what to do with it.
     
-    id: int # unique to each command
-    sent: bool # whether it has been sent to schedule or not
+    Command Types:
+    --------------
+    
+    SEARCH: 
+        - retrieve information about an event.
+        - data is a CalendarEvent whose entries are used as search terms
+        
+    EDIT:
+        - modify an existing event or series of events.
+        - data is a pair of CalendarEvents (tuple):
+            - first's fields are used as search terms (find event to modify)
+            - second is what fields to modify (to leave unchanged, use None)
+    
+    ADD: 
+        - add a new event to the schedule. 
+        - data is the CalendarEvent to add (if repeating, will add all repeats).
+
+    
+    DELETE: 
+        - remove an event or series of events.
+        - data is a CalendarEvent whose search terms are used to find the event.
+    
+    """
+    
     c_type: CommandType
-    event: CalendarEvent
-    event_indices: Tuple[int, int]
+    data: CalendarEvent | Tuple[CalendarEvent, CalendarEvent] # if 2: first is search, 2nd is modifier
     
-    def __init__(self, id:int, c_type:CommandType, event:CalendarEvent, indices:Tuple[int, int]=None):
-        self.id = id
-        self.sent = False
+    def __init__(self, id:int, c_type:CommandType, data:CalendarEvent|Tuple[CalendarEvent, CalendarEvent]):
         self.c_type = c_type
-        self.event = event
-        self.event_indices = indices
+        self.data = data
     
     
     
@@ -47,13 +63,11 @@ class StatusCode(Enum):
 # correlates to exactly one command
 class Response:
     """Holds information about the status of a Command."""
-    command_id: int
     status: StatusCode
     status_details: str
     data = None
     
-    def __init__(self, id:int):
-        self.command_id = id
+    def __init__(self):
         self.status = StatusCode.SUCCESS
         self.status_details = ""
         self.data = None
@@ -63,6 +77,7 @@ class Response:
 # command interpreter
 # transforms text input into a list of commands (to eventually send to the schedule)
 class CommandInterpreter:
+    """Transforms text input into a series of Commands."""
     
     # commands to be sent to the schedule
     commands: List[Command]
@@ -71,57 +86,11 @@ class CommandInterpreter:
         self.commands = list()
     
     # interpret text input and create one or more commands based on it
-    # uses AI model, slow (but can interpret for voice input)
-    def interpret_text(self, text:str):
+    # (use AI model to transform text into list of commands)
+    def generate_commands(self, text:str):
         pass
 
     # manually add command to queue
     # much faster (no AI model use)
     def add_command(self, command:Command):
         self.commands.append(command)
-    
-    
-    # TODO: handle all cases
-    # for now, only handle the showcase test case (success from adding event)
-    def handle_response(self, response:Response):
-        
-        # get corresponding command
-        command: Command = None
-        for c in self.commands:
-            if c.id == response.command_id:
-                command = c
-        
-        if command is None:
-            # error: command was removed before schedule finished processing (shouldn't happen)
-            print("schedule response has no corresponding command")
-            return
-
-        # search, edit, add, delete
-        match command.c_type:
-            case CommandType.SEARCH:
-                pass
-            case CommandType.EDIT:
-                pass
-            case CommandType.ADD:
-                if response.status == StatusCode.SUCCESS:
-                    # success: remove command from list
-                    self.commands.remove(command)
-                else:
-                    pass
-            case CommandType.DELETE:
-                pass
-            case _:
-                print("error: unexpected command type")
-                return
-        
-    
-    def get_next_unsent_command(self) -> Command:
-        index = 0
-        while index < len(self.commands):
-            candidate = self.commands[index]
-            if not candidate.sent:
-                candidate.sent = True
-                return candidate
-            index += 1
-        return None
-        
